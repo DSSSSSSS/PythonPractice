3fd702b888669251d9724db73f9e8f9657f19ad2
aoshunxin <392263014@qq.com>
2018-10-29 21:19:47 +0800

    3

---
+++ Python基础/python练习/网络编程/tcp/server/DataBase/DBtest.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import DBControl as DBC
+
+#pip install sqlalchemy
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
